06d168ec5a7c5125e1a5b049b01641d117ccd669
Programmer Muslim <rizki@imastudio.co.id>
2017-10-18 10:08:44 +0700

    Passing data
    add new view controller

---
+++ Passing Data Segue/Passing Data Segue/GetDataViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GetDataViewController.swift
+//  Passing Data Segue
+//
+//  Created by Rizki Syaputra on 10/18/17.
+//  Copyright © 2017 Rizki Syaputra. All rights reserved.
+//
+
+import UIKit
+
+class GetDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
